df4e7e324651678e8deb645b6dc8997e8606a98f
Miles <mhays7@uwyo.edu>
2024-04-04 13:07:08 -0600

    assigned forms

--- formfiller/lib/get/fetchorgforms.js
+++ formfiller/lib/get/fetchorgforms.js
@@ -10,8 +10,8 @@ module.exports = async function fetchOrgForms(req, res) {
 
     const allForms = await database.query("SELECT * FROM Blank_Forms WHERE Org_Tag = ?", [tag])
 
-    res.json({})
-    return
+    // res.json({})
+    // return
 
     switch (req.query.mode) {
     
@@ -21,13 +21,15 @@ module.exports = async function fetchOrgForms(req, res) {
         var assigned = await database.query("SELECT Blank_Form_ID FROM Assigned_Forms WHERE Email = ?", [user])
         console.log(assigned)
 
-        results = await database.query("SELECT * FROM Blank_Forms WHERE Org_Tag = ?", [tag])
+        res.json(allForms.filter(form => assigned.find(assign => assign.Blank_Form_ID === form.Blank_Form_ID)))
         break
 
     case "submitted":
+        res.json(allForms)
         break
     
     case "draft":
+        res.json(allForms)
         break
 
     case "all":
